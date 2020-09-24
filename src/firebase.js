f9fcd9933954acfd5bb8cc3302e9d972282b9997
chenghsj <43364792+chenghsj@users.noreply.github.com>
2020-09-25 02:01:20 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfZEmrGulF29Y5wDwW2qJEPOS-tDE4zCY",
-  authDomain: "clone-33ba1.firebaseapp.com",
-  databaseURL: "https://clone-33ba1.firebaseio.com",
-  projectId: "clone-33ba1",
-  storageBucket: "clone-33ba1.appspot.com",
-  messagingSenderId: "283479586617",
-  appId: "1:283479586617:web:4c8224b4d3f824ba8c5ae8",
-  measurementId: "G-FJTLB98RNS",
+ 
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
